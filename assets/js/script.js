d7ce7f7b73096b53c285efeec16ea2d8daee8afa
Mike Solo <mike@mikesolodev.es>
2021-09-01 13:24:46 +0200

    commit 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,50 +26,50 @@ window.onscroll = () => {
 
 // })
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-if (!window.AudioContext)
-    yourFirstImplementation();
-else {
-    var buffer,
-        ctx = new AudioContext(),
-        gainNode = ctx.createGain();
-    gainNode.connect(ctx.destination);
-    var vol = document.querySelector('input');
-    vol.value = gainNode.gain.value;
-    vol.addEventListener('change', function () {
-        gainNode.gain.value = this.value;
-    }, false);
+// window.AudioContext = window.AudioContext || window.webkitAudioContext;
+// if (!window.AudioContext)
+//     yourFirstImplementation();
+// else {
+//     var buffer,
+//         ctx = new AudioContext(),
+//         gainNode = ctx.createGain();
+//     gainNode.connect(ctx.destination);
+//     var vol = document.querySelector('input');
+//     vol.value = gainNode.gain.value;
+//     vol.addEventListener('change', function () {
+//         gainNode.gain.value = this.value;
+//     }, false);
 
-    function createBuffer() {
-        ctx.decodeAudioData(this.response, function (b) {
-            buffer = b;
-        }, function (e) { console.warn(e) });
-        var button = document.querySelector('.sound__btn');
-        button.addEventListener('click', function () {
-            playSound(buffer)
-            button.classList.add('play')
-        });
-        button.classList.add('ready');
-    }
+//     function createBuffer() {
+//         ctx.decodeAudioData(this.response, function (b) {
+//             buffer = b;
+//         }, function (e) { console.warn(e) });
+//         var button = document.querySelector('.sound__btn');
+//         button.addEventListener('click', function () {
+//             playSound(buffer)
+//             button.classList.add('play')
+//         });
+//         button.classList.add('ready');
+//     }
 
 
 
-    var file = document.querySelector("#sound"),
-        xhr = new XMLHttpRequest();
-    xhr.onload = createBuffer;
-    xhr.open('GET', file.dataset.src, true);
-    xhr.responseType = 'arraybuffer';
-    xhr.send();
+//     var file = document.querySelector("#sound"),
+//         xhr = new XMLHttpRequest();
+//     xhr.onload = createBuffer;
+//     xhr.open('GET', file.dataset.src, true);
+//     xhr.responseType = 'arraybuffer';
+//     xhr.send();
 
-    function playSound(buf) {
-        var source = ctx.createBufferSource();
-        source.buffer = buf;
-        source.connect(gainNode);
-        source.onended = function () { if (this.stop) this.stop(); if (this.disconnect) this.disconnect(); }
-        source.start(0);
-    }
-}
+//     function playSound(buf) {
+//         var source = ctx.createBufferSource();
+//         source.buffer = buf;
+//         source.connect(gainNode);
+//         source.onended = function () { if (this.stop) this.stop(); if (this.disconnect) this.disconnect(); }
+//         source.start(0);
+//     }
+// }
 
-function yourFirstImplementation() {
-    alert('webAudioAPI is not supported by your browser');
-}
+// function yourFirstImplementation() {
+//     alert('webAudioAPI is not supported by your browser');
+// }
